83c4c00bf1a9b2b8501a778e03ec32ce9ad674de
Marcelo <87618064+Marcelo-2019700081@users.noreply.github.com>
2021-10-14 06:27:31 -0500

    Add files via upload

---
+++ Controllers/tblProductosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC;
+
+namespace MVC.Controllers
+{
+    public class tblProductosController : Controller
+    {
+        private EmpresaEntities2 db = new EmpresaEntities2();
+
+        // GET: tblProductos
+        public ActionResult Index()
+        {
+            return View(db.tblProductos.ToList());
+        }
+
+        // GET: tblProductos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProductos tblProductos = db.tblProductos.Find(id);
+            if (tblProductos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProductos);
+        }
+
+        // GET: tblProductos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblProductos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "codigo,nombre,costo,marca")] tblProductos tblProductos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblProductos.Add(tblProductos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblProductos);
+        }
+
+        // GET: tblProductos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProductos tblProductos = db.tblProductos.Find(id);
+            if (tblProductos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProductos);
+        }
+
+        // POST: tblProductos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "codigo,nombre,costo,marca")] tblProductos tblProductos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblProductos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblProductos);
+        }
+
+        // GET: tblProductos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProductos tblProductos = db.tblProductos.Find(id);
+            if (tblProductos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProductos);
+        }
+
+        // POST: tblProductos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblProductos tblProductos = db.tblProductos.Find(id);
+            db.tblProductos.Remove(tblProductos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/tblVentasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC;
+
+namespace MVC.Controllers
+{
+    public class tblVentasController : Controller
+    {
+        private EmpresaEntities1 db = new EmpresaEntities1();
+
+        // GET: tblVentas
+        public ActionResult Index()
+        {
+            return View(db.tblVentas.ToList());
+        }
+
+        // GET: tblVentas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblVentas tblVentas = db.tblVentas.Find(id);
+            if (tblVentas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblVentas);
+        }
+
+        // GET: tblVentas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblVentas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idventa,nombre_cliente,cantidad,precio_total,fecha_salida")] tblVentas tblVentas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblVentas.Add(tblVentas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblVentas);
+        }
+
+        // GET: tblVentas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblVentas tblVentas = db.tblVentas.Find(id);
+            if (tblVentas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblVentas);
+        }
+
+        // POST: tblVentas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idventa,nombre_cliente,cantidad,precio_total,fecha_salida")] tblVentas tblVentas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblVentas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblVentas);
+        }
+
+        // GET: tblVentas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblVentas tblVentas = db.tblVentas.Find(id);
+            if (tblVentas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblVentas);
+        }
+
+        // POST: tblVentas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblVentas tblVentas = db.tblVentas.Find(id);
+            db.tblVentas.Remove(tblVentas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
